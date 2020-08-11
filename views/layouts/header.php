5d3c197469fc9c052f5a6555661d36e5afded34c
Nata <rahanata9@gmail.com>
2020-08-11 09:36:41 +0700

    add jabatan di header

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,6 +1,7 @@
 <?php
 use yii\helpers\Html;
 use app\modules\app\models\AppUser;
+use app\modules\pegawai\models\JabatanPegawai;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
@@ -13,6 +14,12 @@ use app\modules\app\models\AppUser;
     <?php 
     $id_user = Yii::$app->user->id;
     $model = AppUser::findOne($id_user);
+    $jab_pegawai = JabatanPegawai::find()->where(['id_pegawai'=>$model->pegawai_id, 'status_jbt'=>1])->one();
+    if($jab_pegawai != null){
+        $jabatan = $jab_pegawai->jabatan->nama_jabatan;
+    }else{
+        $jabatan = '-';
+    }
     ?>
 
 
@@ -27,7 +34,7 @@ use app\modules\app\models\AppUser;
             <ul class="nav navbar-nav">
                 <li class="dropdown user user-menu">
 
-                    <a href="" class="dropdown-toggle" data-toggle="dropdown"><span class="hidden-xs"><?php echo $model->pegawai->nama;?></span></a>
+                    <a href="" class="dropdown-toggle" data-toggle="dropdown"><span class="hidden-xs"><?php echo $model->pegawai->nama;?> ( <?php echo $jabatan;?> )</span></a>
                 </li>
             </ul>
         </div>
