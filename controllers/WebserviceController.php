65a961a97f92301a1c583b2f8f7d36c179196dd7
Rahanata <rahanata9@gmail.com>
2020-09-04 14:44:41 +0700

    fix web service

--- controllers/WebserviceController.php
+++ controllers/WebserviceController.php
@@ -9,6 +9,8 @@ use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
 use app\modules\logbook\models\Tugas;
 use app\modules\logbook\models\Kinerja;
+use app\modules\pegawai\models\PegawaiUnitKerja;
+use app\modules\app\models\AppUser;
 use app\models\LoginForm;
 
 class WebserviceController extends Controller
@@ -25,7 +27,7 @@ class WebserviceController extends Controller
 
 	public function actionDatatugas($id)
     {
-    	$model = Tugas::find()->where(['id_unit_kerja'=>$id])->all();
+    	$model = Tugas::find()->where(['id_unit_kerja'=>$id])->orderBy('nama_tugas ASC')->all();
 
     	$arr_json = array();
     	if($model != null){
@@ -50,18 +52,26 @@ class WebserviceController extends Controller
             $tugas = $_POST['tugas'];
             $deskripsi = $_POST['deskripsi'];
             $jumlah = $_POST['jumlah'];
+            $pegawai_id = $_POST['idpeg'];
 
             $explode = explode('|',$tugas);
 
+            $lastmodel = Kinerja::find()->where(['tanggal_kinerja'=>date('Y-m-d'), 'id_pegawai'=>$pegawai_id])->orderBy('row DESC')->one();
+            if($lastmodel != null){
+                $row = $lastmodel->row +1;
+            }else{
+                $row = 0;
+            }
+
             $model = new Kinerja;
             $model->tanggal_kinerja = date('Y-m-d');
-            $model->id_pegawai = 3;
+            $model->id_pegawai = $pegawai_id;
             $model->id_tugas = trim($explode[0]);
             $model->jumlah = $jumlah;
             $model->deskripsi = $deskripsi;
             $model->approval = 0;
             $model->create_date = date('Y-m-d');
-            $model->row = 0;
+            $model->row = $row;
             if($model->save()){
                 $return['error'] = 0;
                 $return['msg'] = 'Kinerja berhasil disimpan';
@@ -85,7 +95,9 @@ class WebserviceController extends Controller
         $model->username = $username;
         $model->password = $password;
         if ($model->login()) {
-            echo 'sukses';
+            $user = AppUser::find()->where(['username'=>$model->username])->one();
+            $peg_unit_kerja = PegawaiUnitKerja::find()->where(['id_pegawai'=>$user->pegawai_id, 'status_peg'=>1])->one();
+            echo $user->pegawai->nama.'#'.$user->pegawai_id.'#'.$peg_unit_kerja->id_unit_kerja;
         }else{
             echo 'gagal';
         }
